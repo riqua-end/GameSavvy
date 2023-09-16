94d0e281890ccd87bbba20b47d46f797d7b31315
dkdlgus <dkdlgusghrm@naver.com>
2023-09-16 18:54:12 +0900

    어드민 카운트 해결 완료

--- gamesavvy/src/main/java/org/ezen/gamesavvy/mapper/AdminMapper.java
+++ gamesavvy/src/main/java/org/ezen/gamesavvy/mapper/AdminMapper.java
@@ -2,6 +2,7 @@ package org.ezen.gamesavvy.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.ezen.gamesavvy.domain.Criteria;
 import org.ezen.gamesavvy.domain.GamesavvyVO;
 import org.ezen.gamesavvy.domain.MemberVO;
@@ -21,15 +22,20 @@ public interface AdminMapper {
   	//관리자 페이지 회원 강제 탈퇴
   	public void deleteMember(String userid);
   	
+  	//권환
   	public void deleteMemberAuth(String userid);
   	
+  	//게시판 
   	public void deleteBoardByWriter(String userid);
+  	
+  	//댓글
+  	public void deleteReplyByReplyer(String userid);
+  	
+  	public void updateReplyCnt(@Param("bno") Long bno, @Param("amount") int amount);
   	
   	//추천
   	//public void deleteLikes(String userid);
   	
-  	//댓글
-  	//public void deleteReplyByWriter(String writer);
 /*====================================================================================*/
 /*=============================== 관리자 게시판 목록 =====================================*/
   	//관리자 페이지 게시판 목록
--- gamesavvy/src/main/java/org/ezen/gamesavvy/service/AdminServiceImpl.java
+++ gamesavvy/src/main/java/org/ezen/gamesavvy/service/AdminServiceImpl.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.ezen.gamesavvy.domain.Criteria;
 import org.ezen.gamesavvy.domain.GamesavvyVO;
 import org.ezen.gamesavvy.domain.MemberVO;
+import org.ezen.gamesavvy.domain.ReplyVO;
 import org.ezen.gamesavvy.mapper.AdminMapper;
+import org.ezen.gamesavvy.mapper.GamesavvyMapper;
+import org.ezen.gamesavvy.mapper.ReplyMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +21,12 @@ public class AdminServiceImpl implements AdminService {
 	
 	@Autowired
 	AdminMapper amapper;
+	
+	@Autowired
+	ReplyMapper rmapper;
+	
+	@Autowired
+	GamesavvyMapper boardmapper;
 	
 	//관리자 페이지 회원 목록
 	@Override
@@ -42,12 +51,20 @@ public class AdminServiceImpl implements AdminService {
 	@Transactional
 	@Override
 	public void removeMember(String userid) {
+		
+		// 댓글 수 업데이트
+	    List<ReplyVO> replies = rmapper.getRepliesByReplyer(userid); // 해당 회원이 작성한 댓글 목록 가져오기
+	    for (ReplyVO reply : replies) {
+	        boardmapper.updateReplyCnt(reply.getBno(), -1); // 댓글 삭제 시 -1
+	    }
+		
 		// 연관 데이터 삭제 순서: LIKE_TEST, TEST_REPLY, TEST_BOARD, TEST_MEMBER_AUTH
 		//amapper.deleteLikes(userid);
-		//amapper.deleteReplyByWriter(userid);
 		amapper.deleteBoardByWriter(userid);
 	    amapper.deleteMemberAuth(userid);
 	    amapper.deleteMember(userid);
+	    amapper.deleteReplyByReplyer(userid);
+	    
 	}
 	
 	//관리자 게시판 관리 페이지 삭제
